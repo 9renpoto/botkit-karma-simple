6d2b8d090ec11ce2fdd5cd349e5bc362a6820bd2
Keisuke Kan <9renpoto@gmail.com>
2017-02-28 23:42:08 +0900

    chore(src) show top karma

--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ type Controller = {
   hears: Function
 }
 
+function format (data: string, length = 2): string {
+  if (data.length === length) {
+    return data
+  }
+
+  if (data.length < length) {
+    length -= data.length
+    for (let i = 0; i < length; i++) {
+      data = '0' + data
+    }
+  }
+  return data
+}
+
 export default class KarmaBot {
   _store: KarmaStore
   _ctrl: Controller
@@ -15,19 +29,38 @@ export default class KarmaBot {
   thingWrapper (thing: string) {
     return thing
   }
-  listen () {
+  _plus () {
     this._ctrl.hears(['([^+\\s])\\+\\+(\\s|$)'], ['ambient'], (bot, msg) => {
       const thing = this.thingWrapper(msg.text.match(/@?(\S+[^+\s])\+\+(\s|$)/)[1].toLowerCase())
-      this._store.up(thing, 1, karma => {
-        bot.reply(msg, `level up! ${thing}: ${karma}`)
+      const n = 1
+      this._store.up(thing, n, karma => {
+        bot.reply(msg, `level up! ${thing}: +${n} (Karma: ${karma})`)
       })
     })
-
+  }
+  _minus () {
     this._ctrl.hears(['([^-\s])--(\s|$)'], ['ambient'], (bot, msg) => {  // eslint-disable-line no-useless-escape
       const thing = this.thingWrapper(msg.text.match(/@?(\S+[^-\s])--(\s|$)/)[1].toLowerCase())
-      this._store.down(thing, 1, karma => {
-        bot.reply(msg, `oops! ${thing}: ${karma}`)
+      const n = 1
+      this._store.down(thing, n, karma => {
+        bot.reply(msg, `oops! ${thing}: -${n} (Karma: ${karma})`)
       })
     })
+  }
+  _showTop () {
+    this._ctrl.hears('karma best', ['direct_mention', 'mention'], (bot, msg) => {
+      this._store.top(10, (top: string[]) => {
+        const response = [`The Best:`]
+        for (let i = 0; i < top.length; i += 2) {
+          response.push(`${format((i / 2 + 1).toString())}. ${this.thingWrapper(top[i])}: ${top[i + 1]}`)
+        }
+        bot.reply(msg, response.join('\n'))
+      })
+    })
+  }
+  listen () {
+    this._plus()
+    this._minus()
+    this._showTop()
   }
 }

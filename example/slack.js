b73e0cab14cbbefb6d8dff45496a11adc0e7508c
Keisuke Kan <9renpoto@gmail.com>
2017-05-07 13:23:05 +0900

    chore(package) use prettier

--- example/slack.js
+++ example/slack.js
@@ -1,13 +1,15 @@
 const KarmaBot = require('../lib/index').default
 const Botkit = require('botkit')
 const controller = Botkit.slackbot()
-controller.spawn({
-  token: process.env.token
-}).startRTM((err, bot, payload) => {
-  if (err) {
-    throw new Error('Could not connect to Slack')
-  }
-})
+controller
+  .spawn({
+    token: process.env.token
+  })
+  .startRTM((err, bot, payload) => {
+    if (err) {
+      throw new Error('Could not connect to Slack')
+    }
+  })
 
 const instance = new KarmaBot(controller)
 instance.listen()

fbd308f029c7b1db0ececb2abd1197ccf55cf0bc
Keisuke Kan <9renpoto@gmail.com>
2019-09-22 12:08:58 +0900

    Upgrade env

--- example/slack.js
+++ example/slack.js
@@ -1,15 +1,16 @@
-const KarmaBot = require('../lib/index').default
-const Botkit = require('botkit')
-const controller = Botkit.slackbot()
+const Botkit = require("botkit");
+const KarmaBot = require("../lib").default;
+const controller = Botkit.slackbot();
+
 controller
   .spawn({
     token: process.env.token
   })
   .startRTM((err, bot, payload) => {
     if (err) {
-      throw new Error('Could not connect to Slack')
+      throw new Error("Could not connect to Slack");
     }
-  })
+  });
 
-const instance = new KarmaBot(controller)
-instance.listen()
+const instance = new KarmaBot(controller);
+instance.listen();
